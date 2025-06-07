b0c0d24bce6b091a45175583412f40ef8c6f3906
Saharyoussef <youssefsahar86@gmail.com>
2025-06-07 11:37:35 +0100

    change the photo directory constant

--- userservice/src/main/java/com/sahar/userservice/constant/Constants.java
+++ userservice/src/main/java/com/sahar/userservice/constant/Constants.java
@@ -2,5 +2,5 @@ package com.sahar.userservice.constant;
 
 public class Constants {
     public static final String WEVIOO = "WEVIOO";
-    public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
+    public static final String PHOTO_DIRECTORY = "C:\\Users\\Sahar Y\\Downloads\\";
 }
