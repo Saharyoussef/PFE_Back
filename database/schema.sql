224da9f028faee58f2adce7532ab45875c98356b
Saharyoussef <youssefsahar86@gmail.com>
2025-06-07 11:33:21 +0100

    add the update profile function

--- database/schema.sql
+++ database/schema.sql
@@ -236,6 +236,24 @@ CREATE OR REPLACE FUNCTION update_user_role (IN p_user_uuid VARCHAR(40), IN p_ro
     END;
     $$
 
+CREATE OR REPLACE FUNCTION update_user_profile (IN p_user_uuid VARCHAR(40), IN p_first_name VARCHAR(25), IN p_last_name VARCHAR(25), IN p_email VARCHAR(40), IN p_phone VARCHAR(15),IN p_bio VARCHAR(100),IN p_address VARCHAR(100))
+    RETURNS TABLE(qr_code_image_uri TEXT, member_id VARCHAR, role VARCHAR, authorities TEXT, account_non_expired BOOLEAN, account_non_locked BOOLEAN, created_at TIMESTAMP WITH TIME ZONE, email VARCHAR, enabled BOOLEAN, first_name VARCHAR, user_id BIGINT, image_url VARCHAR, last_login TIMESTAMP WITH TIME ZONE, last_name VARCHAR, mfa BOOLEAN, updated_at TIMESTAMP WITH TIME ZONE, user_uuid VARCHAR, phone VARCHAR, bio VARCHAR, address VARCHAR)
+    LANGUAGE PLPGSQL
+    AS $$
+    BEGIN
+        UPDATE users u SET 
+            first_name = p_first_name, 
+            last_name = p_last_name, 
+            email = p_email, 
+            phone = p_phone, 
+            bio = p_bio, 
+            address = p_address, 
+            updated_at = NOW() 
+        WHERE u.user_uuid = p_user_uuid;
+        RETURN QUERY SELECT u.qr_code_image_uri, u.member_id, r.name AS role, r.authority AS authorities, u.account_non_expired, u.account_non_locked, u.created_at, u.email, u.enabled, u.first_name, u.user_id, u.image_url, u.last_login, u.last_name, u.mfa, u.updated_at, u.user_uuid, u.phone, u.bio, u.address FROM users u JOIN user_roles ur ON ur.user_id = u.user_id JOIN roles r ON r.role_id = ur.role_id WHERE u.user_uuid = p_user_uuid;
+    END;
+    $$
+
 
 CREATE OR REPLACE FUNCTION create_grafanadashboard (
     IN p_grafanadashboard_uuid VARCHAR(40),
